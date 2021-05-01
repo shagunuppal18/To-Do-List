8efa9da5df9a08e6cdc25ad96c0de178f8f0a093
Shagun Uppal <uppalshagun18@gmail.com>
2021-05-01 15:04:02 +0530

    Initial Commit

---
+++ taskmate/urls.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from todolist import views as todolist_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', todolist_views.index , name= 'index'),
+    path('todolist/', include('todolist.urls')),
+    path('account/', include('users_app.urls')),
+    path('contact', todolist_views.contact , name= 'contact'),
+    path('about-us', todolist_views.about , name= 'about'),
+    
+]
